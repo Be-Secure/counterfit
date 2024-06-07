b9bd4d5a5e9361ff47cb6107b0a369a4428d3eb4
Ubuntu <ubuntu@ip-172-31-15-80.ap-south-1.compute.internal>
2024-06-07 07:08:55 +0000

    removed onboarded targets

--- counterfit/targets/__init__.py
+++ counterfit/targets/__init__.py
@@ -4,5 +4,4 @@ from .digits_keras import DigitKeras
 from .movie_reviews import MovieReviewsTarget
 from .satellite import SatelliteImages
 from .cart_pole import CartPoleInitState, CartPole
-from .testtarget import Testtarget
-from .bes_image_classification import Bes_image_classification
+
